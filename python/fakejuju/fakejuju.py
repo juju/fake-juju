bc72b7294b26aeccbb61ff9969d677fd2f319dfb
Eric Snow <eric.snow@canonical.com>
2016-10-06 22:00:25 +0000

    Add fakejuju.get_filename().

--- python/fakejuju/fakejuju.py
+++ python/fakejuju/fakejuju.py
@@ -1,5 +1,7 @@
 # Copyright 2016 Canonical Limited.  All rights reserved.
 
+import os.path
+
 import txjuju.cli
 
 
@@ -12,3 +14,19 @@ def get_bootstrap_spec(name, admin_secret=None):
     type = "dummy"
     default_series = None  # Use the default.
     return txjuju.cli.BootstrapSpec(name, type, default_series, admin_secret)
+
+
+def get_filename(version, bindir=None):
+    """Return the full path to the fake-juju binary for the given version.
+
+    @param version: The Juju version to use.
+    @param bindir: The directory containing the fake-juju binary.
+        This defaults to /usr/bin.
+    """
+    if not version:
+        raise ValueError("version not provided")
+    filename = "fake-juju-{}".format(version)
+    if bindir is None:
+        # XXX Search $PATH.
+        bindir = "/usr/bin"
+    return os.path.join(bindir, filename)
--- python/fakejuju/tests/test_fakejuju.py
+++ python/fakejuju/tests/test_fakejuju.py
@@ -4,7 +4,7 @@ import unittest
 
 import txjuju.cli
 
-from fakejuju.fakejuju import get_bootstrap_spec
+from fakejuju.fakejuju import get_bootstrap_spec, get_filename
 
 
 class HelperTests(unittest.TestCase):
@@ -20,3 +20,24 @@ class HelperTests(unittest.TestCase):
         spec = get_bootstrap_spec("my-env")
 
         self.assertEqual(spec, txjuju.cli.BootstrapSpec("my-env", "dummy"))
+
+    def test_get_filename_full(self):
+        filename = get_filename("1.25.6", "/spam")
+
+        self.assertEqual(filename, "/spam/fake-juju-1.25.6")
+
+    def test_get_filename_minimal(self):
+        filename = get_filename("1.25.6")
+
+        self.assertEqual(filename, "/usr/bin/fake-juju-1.25.6")
+
+    def test_get_filename_empty_bindir(self):
+        filename = get_filename("1.25.6", "")
+
+        self.assertEqual(filename, "fake-juju-1.25.6")
+
+    def test_get_filename_missing_version(self):
+        with self.assertRaises(ValueError):
+            get_filename(None)
+        with self.assertRaises(ValueError):
+            get_filename("")
