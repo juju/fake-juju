855339ec330e64e69c573507f49ab75c9a63d451
Eric Snow <eric.snow@canonical.com>
2016-10-13 18:25:28 +0000

    Split the helper tests into separate test classes.

--- python/fakejuju/tests/test_fakejuju.py
+++ python/fakejuju/tests/test_fakejuju.py
@@ -5,35 +5,38 @@ import unittest
 from fakejuju.fakejuju import get_filename, set_envvars
 
 
-class HelperTests(unittest.TestCase):
+class GetFilenameTests(unittest.TestCase):
 
-    def test_get_filename_full(self):
+    def test_all_args(self):
         """get_filename() works correctly when given all args."""
         filename = get_filename("1.25.6", "/spam")
 
         self.assertEqual(filename, "/spam/fake-juju-1.25.6")
 
-    def test_get_filename_minimal(self):
+    def test_minimal_args(self):
         """get_filename() works correctly when given minimal args."""
         filename = get_filename("1.25.6")
 
         self.assertEqual(filename, "/usr/bin/fake-juju-1.25.6")
 
-    def test_get_filename_empty_bindir(self):
+    def test_empty_bindir(self):
         """get_filename() works correctly when given an empty string
         for bindir."""
         filename = get_filename("1.25.6", "")
 
         self.assertEqual(filename, "fake-juju-1.25.6")
 
-    def test_get_filename_missing_version(self):
+    def test_missing_version(self):
         """get_filename() fails if version is None or empty."""
         with self.assertRaises(ValueError):
             get_filename(None)
         with self.assertRaises(ValueError):
             get_filename("")
 
-    def test_set_envvars_full(self):
+
+class SetEnvvarsTests(unittest.TestCase):
+
+    def test_all_args(self):
         """set_envvars() works correctly when given all args."""
         envvars = {}
         set_envvars(envvars, "/spam/failures", "/eggs/logsdir")
@@ -43,7 +46,7 @@ class HelperTests(unittest.TestCase):
             "FAKE_JUJU_LOGS_DIR": "/eggs/logsdir",
             })
 
-    def test_set_envvars_minimal(self):
+    def test_minimal_args(self):
         """set_envvars() works correctly when given minimal args."""
         envvars = {}
         set_envvars(envvars)
@@ -53,7 +56,7 @@ class HelperTests(unittest.TestCase):
             "FAKE_JUJU_LOGS_DIR": "",
             })
 
-    def test_set_envvars_start_empty(self):
+    def test_start_empty(self):
         """set_envvars() sets all values on an empty dict."""
         envvars = {}
         set_envvars(envvars, "x", "y")
@@ -63,7 +66,7 @@ class HelperTests(unittest.TestCase):
             "FAKE_JUJU_LOGS_DIR": "y",
             })
 
-    def test_set_envvars_no_collisions(self):
+    def test_no_collisions(self):
         """set_envvars() sets all values when none are set yet."""
         envvars = {"SPAM": "eggs"}
         set_envvars(envvars, "x", "y")
@@ -74,7 +77,7 @@ class HelperTests(unittest.TestCase):
             "FAKE_JUJU_LOGS_DIR": "y",
             })
 
-    def test_set_envvars_empty_to_nonempty(self):
+    def test_empty_to_nonempty(self):
         """set_envvars() updates empty values."""
         envvars = {
             "FAKE_JUJU_FAILURES": "",
@@ -87,7 +90,7 @@ class HelperTests(unittest.TestCase):
             "FAKE_JUJU_LOGS_DIR": "y",
             })
 
-    def test_set_envvars_nonempty_to_nonempty(self):
+    def test_nonempty_to_nonempty(self):
         """set_envvars() overwrites existing values."""
         envvars = {
             "FAKE_JUJU_FAILURES": "spam",
@@ -100,7 +103,7 @@ class HelperTests(unittest.TestCase):
             "FAKE_JUJU_LOGS_DIR": "y",
             })
 
-    def test_set_envvars_nonempty_to_empty(self):
+    def test_nonempty_to_empty(self):
         """set_envvars() with no args "unsets" existing values."""
         envvars = {
             "FAKE_JUJU_FAILURES": "x",
