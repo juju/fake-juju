6d31bbbd46088946e17b9a5a48ce0a85c9493960
Eric Snow <eric.snow@canonical.com>
2016-10-18 20:54:27 +0000

    Fix up FakeJujuFixture.

--- python/fakejuju/testing.py
+++ python/fakejuju/testing.py
@@ -1,6 +1,5 @@
 # Copyright 2016 Canonical Limited.  All rights reserved.
 
-import txjuju
 from fixtures import Fixture, TempDir
 from testtools.content import content_from_file
 
@@ -40,29 +39,16 @@ class FakeJujuFixture(Fixture):
     def setUp(self):
         super(FakeJujuFixture, self).setUp()
         self._juju_home = self.useFixture(TempDir())
-        self._juju = fakejuju.FakeJuju.make(
-            self._juju_home.path, self._version, self._logs_dir)
+        self.fakejuju = fakejuju.FakeJuju.from_version(
+            self._version, self._juju_home.path, self._logs_dir)
 
         if not self._logs_dir:
             # Attach logs as testtools details.
             self.addDetail("log-file", content_from_file(self._juju.logfile))
 
-        spec = fakejuju.get_bootstrap_spec(self._controller, self._password)
-        cfgfile = txjuju.prepare_for_bootstrap(
-            spec, self._version, self._juju_home)
-        cli = self._juju.cli()
-        cli.bootstrap(spec, cfgfile=cfgfile)
-        api_info = cli.api_info(spec.name)
-        if self._version.startswith("1."):
-            # fake-juju doesn't give us the password, so we have to
-            # set it here.
-            api_info = api_info._replace(password=self._password)
-        self.api_info = api_info
+        self._juju, self.all_api_info = self.fakejuju.bootstrap(
+            self._controller, self._password)
 
     def cleanUp(self):
         self._juju.destroy_controller(self._controller)
         super(FakeJujuFixture, self).cleanUp()
-
-    def add_failure(self, entity):
-        """Make the given entity fail with an error status."""
-        self._juju.failures.fail_entity(entity)
