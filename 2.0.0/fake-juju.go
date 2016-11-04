958a16c8bbda676f925e367d939e493cf1b9afe7
Eric Snow <eric.snow@canonical.com>
2016-11-04 15:26:51 +0000

    Allow internet access.

--- 2.0.0/fake-juju.go
+++ 2.0.0/fake-juju.go
@@ -562,6 +562,26 @@ var _ = gc.Suite(&FakeJujuSuite{})
 func (s *FakeJujuSuite) SetUpTest(c *gc.C) {
 	s.JujuConnSuite.SetUpTest(c)
 
+	// Juju needs internet access to reach the charm store.  This is
+	// necessary to download charmstore charms (e.g. when adding a
+	// service.  In the case of downloading charms, the controller
+	// does not try to download if the charm is already in the database.
+	// So internet access could go back to disabled if we were to add
+	// the charm directly before using any API methods that try to
+	// download it.  Here are some ideas on how to do that:
+	//  * use the API's "upload charm" HTTP endpoint (POST on /charms);
+	//    this requires disabling the prohibition against uploading
+	//    charmstore charms (which we've done already in fake-juju);
+	//    this could be accomplished using txjuju, python-jujuclient,
+	//    or manually with httplib (all have auth complexity)
+	//  * add an "upload-charm" command to fake-juju that forces a
+	//    charmstore charm into the DB
+	//  * add the service with a "local" charm schema and then forcibly
+	//    change the charm's schema to "cs" in the DB
+	// In the meantime, we allow fake-juju to have internet access.
+	// XXX (lp:1639276): Remove this special case.
+	utils.OutgoingAccessAllowed = true
+
 	c.Assert(os.Getenv(envDataDir), gc.Not(gc.Equals), "")
 	s.filenames = newFakeJujuFilenames("", "", "")
 	// Note that LoggingSuite.SetUpTest (github.com/juju/testing/log.go),
